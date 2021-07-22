de348a71d77664306331299f9ead33cc1b204c80
winkymonkey <pal.abhishek.2008@gmail.com>
2021-07-22 11:49:08 +0530

    refactor

--- src/main/java/org/example/demo/MyConfigClientApplication.java
+++ src/main/java/org/example/demo/MyConfigClientApplication.java
@@ -1,11 +1,11 @@
-package com.example.demo;
+package org.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class MyConfigClientApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MyConfigClientApplication.class, args);
 	}
